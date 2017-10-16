3bbf8016e29d517d13e97a933592c54e37340497
Sean <seankao31@gmail.com>
2017-10-17 02:55:08 +0800

    readn

---
+++ serverlib.cpp
@@ -0,0 +1,28 @@
+#include <errno.h>
+#include <cstdlib>
+#include <unistd.h>
+
+ssize_t readn(int sockfd, char *vptr, size_t n) {
+    size_t nleft;
+    ssize_t nread;
+    char *ptr;
+
+    ptr = vptr;
+    nleft = n;
+    while (nleft > 0) {
+        if ((nread = read(sockfd, ptr, nleft)) < 0) {
+            if (errno == EINTR) {
+                nread = 0;
+            }
+            else {
+                return -1;
+            }
+        }
+        else if (nread == 0)
+            break;
+
+        nleft -= nread;
+        ptr += nread;
+    }
+    return n - nleft;
+}
