ee4d87a165ecc11a22660d97470a3fbe9ab48ca5
Sean <seankao31@gmail.com>
2017-10-19 22:34:49 +0800

    change struct command

--- shell.h
+++ shell.h
@@ -13,16 +13,6 @@
 
 int sockfd;
 
-struct command_segment {
-    char *args[ARGSIZE];
-    struct command_segment *next;
-};
-
-struct command {
-    struct command_segment *root;
-    int pipe_to;
-};
-
 void welcome() {
     std::string greet = "****************************************\n"
                         "** Welcome to the information server. **\n"
@@ -42,6 +32,55 @@ void err_dump(const char* str) {
     writen(sockfd, str, strlen(str));
 }
 
+void log(const std::string str) {
+    writen(sockfd, str.c_str(), str.length());
+}
+
+void log(const char* str) {
+    writen(sockfd, str, strlen(str));
+    writen(sockfd, str, strlen(str) * sizeof(char));
+}
+
+struct command {
+    char *args[ARGSIZE];
+    struct command_segment *next;
+    int pipe_to;
+};
+
+struct command* parse_command(std::string line) {
+    //struct command *cmd = new struct command();
+    struct command *root = new struct command();
+    struct command *cur = root;
+
+    char *sep = new char[line.length() + 1];
+    std::copy(line.begin(), line.end(), sep);
+    sep[line.length()] = '\0';
+    char *segment;
+    int argc = 0;
+    segment = strsep(&sep, "|");
+    //for (argc = 0; argc < ARGSIZE && (cur->args[argc] = strtok(segment, TOKEN_DELIMITERS)) != NULL; argc++)
+        //segment = NULL;
+    //cur->args[argc] = NULL;
+    //log(sep);
+    log(segment);
+
+    //while ((segment = strsep(&sep, "|")) != NULL) {
+        //struct command_segment *cmd_seg = malloc(sizeof(struct command_segment));
+        //cur = cur->next = cmd_seg;
+        //for (argc = 0; argc < ARGSIZE - 1 && (cur->args[argc] = strtok(segment, TOKEN_DELIMITERS)) != NULL; argc++)
+            //segment = NULL;
+        //cur->args[argc] = NULL;
+    //}
+    //cur->next = NULL;
+
+    //delete[] sep;
+    return root;
+}
+
+int execute_command(struct command *command) {
+    return 0;
+}
+
 void loop() {
     std::string line;
     struct command *command;
@@ -53,8 +92,8 @@ void loop() {
         if (line.length() == 0) {
             continue;
         }
-        err_dump(line);
-        err_dump(line.c_str());
+        command = parse_command(line.c_str());
+        status = execute_command(command);
     } while (status >= 0);
 }
 
